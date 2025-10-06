2ea2ed4cc3e5743097b3e6ee4346c804d3c368ef
justputoff <kholid.hasibuan75@gmail.com>
2025-10-06 09:16:37 +0700

    Update TransactionController.php

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -279,7 +279,10 @@ class TransactionController extends Controller
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'POST',
             CURLOPT_POSTFIELDS => array(
-                'target' => '6281261686210', '6282288513102', $transaction->merchant->phone,
+                'target' => '6281261686210',
+                'target' => '6281261686210',
+                'target' => '6282288513102',
+                'target' => $transaction->merchant->phone,
                 'message' => $message
             ),
             CURLOPT_HTTPHEADER => array(
